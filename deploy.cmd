533d36366808a5a54db20e5d5edb7aacc03fd4bb
nojaf <florian.verdonck@outlook.com>
2017-11-20 23:11:52 +0100

    Building in Release and added web.config

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 %DEPLOYMENT_SOURCE%\.paket\paket.exe install
-dotnet build %DEPLOYMENT_SOURCE%\html-to-fsharp.sln
+dotnet build %DEPLOYMENT_SOURCE%\html-to-fsharp.sln -c Release
 dotnet publish %DEPLOYMENT_SOURCE%\src\HtmlToFSharp.Web\ -c Release
 xcopy %DEPLOYMENT_SOURCE%\src\HtmlToFSharp.Web\bin\Release\net461\publish\* %DEPLOYMENT_TARGET% /Y
